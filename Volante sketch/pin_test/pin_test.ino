cf2ff59f23c523cc76145424e70f153062c4f4af
Andrea Mancini <mancioman2@gmail.com>
2018-07-12 16:29:10 +0200

    update

--- Volante sketch/pin_test/pin_test.ino
+++ Volante sketch/pin_test/pin_test.ino
@@ -1,12 +1,12 @@
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-        pinMode(14,INPUT_PULLUP);
+        pinMode(1,INPUT);
 
 }
 
 void loop() {
 
-        int x = digitalRead(14);
+        int x = digitalRead(1);
         Serial.println(x);
         
 }
